362c119a9f52d17841efeb51d4b3670b0e2b13db
muratayguney <dug.ZWK@axe7pfa4dmq>
2025-05-29 09:53:39 +0300

    ffmpeg stream

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function ffmpegCommand() {
         ffmpeg = child_process.spawn("ffmpeg", [
             '-f', 'v4l2',
             '-framerate', '25',
-            '-video_size', '640x480',
+            '-video_size', '1280x720',
             '-i', '/dev/video0',
             '-f', 'mjpeg',
             '-q:v', '5',
